44dd96656b2791139f6a4cada05f95917bb448a2
wafaa <wafaa.wadea@uittai.com>
2024-11-04 00:46:51 +0200

    fix type err at apis functions

--- features/posts/apis/getAllPosts.ts
+++ features/posts/apis/getAllPosts.ts
@@ -1,7 +1,7 @@
 import { AxiosCreate } from "@/app/api/AxiosCreate";
 import { host_url } from "@/config/config";
 
-export async function getAllPosts(skip=0,search=''){
+export async function getAllPosts(skip=0, search=''){
     const res = await AxiosCreate.get(`${host_url}posts/search?q=${search}&limit=10&skip=${skip}`);
     return res;
 }
--- features/posts/apis/updatePost.ts
+++ features/posts/apis/updatePost.ts
@@ -1,6 +1,6 @@
 import { AxiosCreate } from "@/app/api/AxiosCreate";
 import { host_url } from "@/config/config";
 
-export async function updatePost(data:{},id:string) {
+export async function updatePost(data, id) {
     return await AxiosCreate.put(`${host_url}posts/${id}`, data);
 }
